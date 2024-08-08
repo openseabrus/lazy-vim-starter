68af2077b205f69cab27961f25d6346fc897c76f
gse@powerdot.eu <141630631+seabrus-pwdt@users.noreply.github.com>
2024-08-08 16:10:30 +0100

    cattpuccin

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,11 @@
+return {
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, opts = {
+    flavour = "macchiato",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin",
+    },
+  },
+}
